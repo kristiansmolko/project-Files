c870ca75d304dd80283f228f2666ba42407c965e
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-11 10:19:14 +0100

    Added project Files

--- src/files/ReadFileLine.java
+++ src/files/ReadFileLine.java
@@ -1,24 +1,39 @@
 package files;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ReadFileLine {
     public void copyFileWithCapital(String fileName){
         BufferedReader reader;
         try {
             FileReader fr = new FileReader(fileName);
+
+            File file = new File("resource/" + getFileName());
+            file.createNewFile();
+
+            FileWriter fw = new FileWriter(file);
             reader = new BufferedReader(fr);
             String line;
             while ((line=reader.readLine())!=null){
                 line = line.toUpperCase();
-                System.out.println(line);
+                fw.write(line +"\n");
             }
 
             fr.close();
+            fw.close();
         }catch(IOException ex){
             ex.printStackTrace();
         }
+    }
+
+    private String getFileName(){
+        Date today = new Date();
+        DateFormat sdf = new SimpleDateFormat("HHmmss");
+        String name;
+        name = "b_" + sdf.format(today);
+        return name;
     }
 }
