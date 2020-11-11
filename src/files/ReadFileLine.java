1db69248a7a17aaa5391604c95bbac3a2b99a55c
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-11 10:00:00 +0100

    Added project Files

---
+++ src/files/ReadFileLine.java
@@ -0,0 +1,22 @@
+package files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLine {
+    public void copyFileWithCapital(String fileName){
+        BufferedReader reader;
+        try {
+            FileReader fr = new FileReader(fileName);
+            reader = new BufferedReader(fr);
+            String line;
+            while ((line=reader.readLine())!=null){
+                line = line.toUpperCase();
+                System.out.println(line);
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
